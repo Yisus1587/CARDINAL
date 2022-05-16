fcc8abfa8ab6228f61d9b522a1000c88d43d72c1
Yisus1587 <105611431+Yisus1587@users.noreply.github.com>
2022-05-15 19:00:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('GATABOT V2\nPOR\nGATA DIOS', {
+CFonts.say('CARDINAL /n Yisus1587', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-CFonts.say(`¡Hola! espero estes teniendo un momento`, {
+CFonts.say(`¡Hola! espera un momento`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
